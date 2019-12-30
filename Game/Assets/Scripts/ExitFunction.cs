aa91cb9a8be61823694e310170e851fb0671e651
Himanshu Garg <35988194+merrcury@users.noreply.github.com>
2019-12-30 22:09:16 +0530

    Unity App Initalize v0.10

---
+++ Game/Assets/Scripts/ExitFunction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitFunction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Update () {
+
+    if (Application.platform == RuntimePlatform.Android) {
+
+        if (Input.GetKeyUp(KeyCode.Escape)) {
+
+           //quit application on return button
+
+           Application.Quit();
+
+           return;
+
+       }
+
+    }
+}
+}
---
+++ Game/Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,17 @@
+﻿using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith( continuationAction: task => {
+          FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+
+
+    }
+}
